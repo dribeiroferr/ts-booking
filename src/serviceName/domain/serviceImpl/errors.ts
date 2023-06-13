62525957600f1677f0697070e2148886c3d65c9b
dribeiroferr <dribeiroferr.ito@gmail.com>
2023-06-13 01:41:29 -0300

    feat: adding base project

--- src/serviceName/domain/serviceImpl/errors.ts
+++ src/serviceName/domain/serviceImpl/errors.ts
@@ -3,6 +3,7 @@ import { ErrorBase } from "../../../utils/errors/errors";
 type ErrorName = 
     | 'GET_PROJECT_ERROR'
     | 'CREATE_PROJECT_ERROR'
-    | 'PROJECT_LIMIT_REACHED';
+    | 'PROJECT_LIMIT_REACHED'
+    | 'UPDATE_PROJECT_ERROR'
 
 export class TeamError extends ErrorBase<ErrorName> {}
